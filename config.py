2fa6bd9bfb92ecb8eb5d826d94ded71cf3e7acd6
Czam <inkczam@gmail.com>
2026-07-30 02:01:01 +0000

    add puta to bad words

--- config.py
+++ config.py
@@ -1,21 +1,5 @@
-import os
-
-TOKEN = os.getenv("DISCORD_TOKEN", "YOUR_BOT_TOKEN")
-RESET_HOUR = 15
-RESET_MINUTE = 59
-FINE_PER_SWEAR = 50
-
-BAD_WORDS = [
-    # Multi-word first (longest match priority)
-    "putangina mo", "potangina mo", "tangina mo", "fuck you",
-    "mamatay ka na",
-    # Single words
-    "putangina", "potangina", "tangina",
-    "ampota", "kingina", "pota",
-    "taena", "taina",
-    "tarantado", "rantado",
-    "gago", "tanga", "bobo", "tado",
-    "8080", "pakyu",
-    "fuck", "shit", "bitch", "asshole", "damn", "hell",
-    "hayop", "haup"
-]
+TOKEN = ""
+RESET_HOUR = 0
+RESET_MINUTE = 0
+BAD_WORDS = ["hayop", "haup", "puta"]
+FINE_PER_SWEAR = 20
